dc565377b2b305575aa334389e69356565add0dd
Anastasia <156694058+Anastasia-Klestova@users.noreply.github.com>
2024-03-19 12:24:48 +0700

    Update MantisTest.java

--- MantisTest.java
+++ MantisTest.java
@@ -1,11 +1,12 @@
 package ru.academits.mantis;
 
 import io.restassured.RestAssured;
-import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -55,25 +56,27 @@ public class MantisTest {
 
     @Test
     public void updateRealNameTest() {
-        String RealName = "Nuw Real Name";
+        String RealName = "new Real Name";
         Timestamp timestamp = new Timestamp(new Date().getTime());
+        String newRealName = "admin1" + timestamp;
+
 
         Response responseUpdateName = given()
                 .contentType("application/x-www-form-urlencoded")
                 .cookies(cookies)
-                .body("real_name" + RealName + "password_current=&password=&password_confirm=&email=rov55an3014%40mail.ru&realname=admin1"+timestamp)
+                .body("real_name" + RealName + "password_current=&password=&password_confirm=&email=rovan3014%40mail.ru&realname=" + newRealName)
                 .when()
                 .post("https://academ-it.ru/mantisbt/account_update.php")
                 .andReturn();
         responseUpdateName.prettyPrint();
-        assertNotEquals("Realname", "Nuw Real Name");
         assertTrue(responseUpdateName.body().asString().contains("Real name successfully updated"));
-
-        Response response = given()
-                .cookies(cookies)
-                .get("https://academ-it.ru/mantisbt/account_page.php")
-                .andReturn();
-        JsonPath.from("Real Name = admin3timestamp");
+        Response response = RestAssured.get("https://academ-it.ru/mantisbt/account_page.php");
+        String realName = response.getBody().asString();
+        if (!newRealName.equals(realName)) {
+            System.out.println("Значения различаются!");
+        } else {
+            System.out.println("Значения совпадают.");
+        }
 
     }
 }
